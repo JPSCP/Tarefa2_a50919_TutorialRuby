cf6c1fd7ab9a2c9d06788c38f734d4dff593b3b0
JPSCP <a50919@alunos.ipb.pt>
2026-03-24 12:33:35 +0000

    Set up a 2D player character 7-11
    7. Add code to make the player character move
    8. Test your changes
    9. More things to try
    10. Check your script
    11. Next steps

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,16 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 position = transform.position;
+        position.x = position.x + 0.001f;
+        position.y = position.y + 0.001f;
+        transform.position = position;
     }
 }
