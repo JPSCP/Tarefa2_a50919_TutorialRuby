07cfbf228f0c587c22365286a367c8ec3c4d4f74
JPSCP <a50919@alunos.ipb.pt>
2026-03-27 14:20:09 +0000

    Set up a basic health system
    1. Overview
    2. Add new variables to store health
    3.Write a new function to change health
    4. Check the maxHealth variable in the Inspector
    5. Expose a variable to control player character speed
    6. Check your script
    7. Next steps

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,12 +8,20 @@ public class PlayerController : MonoBehaviour
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
+    public float speed = 3.0f;
+
+
+    // Variables related to the health system
+    public int maxHealth = 5;
+    int currentHealth;
+
 
     // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -23,9 +31,19 @@ public class PlayerController : MonoBehaviour
         Debug.Log(move);
     }
 
+
+    // FixedUpdate has the same call rate as the physics system 
     void FixedUpdate()
     {
-        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
         rigidbody2d.MovePosition(position);
     }
+
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
 }
