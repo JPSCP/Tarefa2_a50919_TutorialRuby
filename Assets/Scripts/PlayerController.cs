efe45090db3112db11eae2ecfabee5a5602f561e
JPSCP <a50919@alunos.ipb.pt>
2026-04-04 19:27:26 +0100

    Play animations based on game data 4-8
    4. Review the PlayerCharacter’s Animator Controller
    5. Adjust the PlayerController script
    6. Pass data to the Animator parameters
    7. Check your scripts
    8. Next steps

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,14 +22,17 @@ public class PlayerController : MonoBehaviour
     bool isInvincible;
     float damageCooldown;
 
+    // Variables for animation
+    Animator animator;
+    Vector2 moveDirection = new Vector2(1, 0);
+
 
     // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
-
-
+        animator = GetComponent<Animator>();
         currentHealth = maxHealth;
     }
 
@@ -45,6 +48,17 @@ public class PlayerController : MonoBehaviour
             if (damageCooldown < 0)
                 isInvincible = false;
         }
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            moveDirection.Set(move.x, move.y);
+            moveDirection.Normalize();
+
+        }
+
+        animator.SetFloat("Look X", moveDirection.x);
+        animator.SetFloat("Look Y", moveDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
     }
 
 
@@ -65,6 +79,7 @@ public class PlayerController : MonoBehaviour
 
             isInvincible = true;
             damageCooldown = timeInvincible;
+            animator.SetTrigger("Hit");
         }
 
 
